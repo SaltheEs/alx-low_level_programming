232a582ff0fd4e42534a4e5d5e113a8bfe30b7de
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-16 19:33:12 -0700

    last commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,7 @@
 */
 
 int main(void)
-{
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+{	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(2, quote, 59);
 	return (1);
