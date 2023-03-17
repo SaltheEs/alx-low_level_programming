1852102cf7923fb17d0d5ab67f5e3cfabc5b1d10
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-16 18:38:35 -0700

    Data types sizes task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLIntType));
-	printf("Size of a float: %zu byte(s)\n", sizeof(floatType;));
+	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 	return (0);
 }
