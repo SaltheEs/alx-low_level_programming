8ab95f9289a73cd6288321ff7f9170a1b40d67d6
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-16 19:25:51 -0700

    last commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this programme will print with write functionin unistd
+ *
+ * Return: 1 (error)
+*/
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, quote, 59);
+	return (1);
+}
