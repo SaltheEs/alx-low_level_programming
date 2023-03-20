546f000a5ceed73fcc66dbad310e869d93583a72
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-20 16:29:04 -0700

    Task 3: putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this programme will print with puts and not printf
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	scanf("%c", &letter);
+	putchar(tolower(letter));
+
+	return (0);
+}
