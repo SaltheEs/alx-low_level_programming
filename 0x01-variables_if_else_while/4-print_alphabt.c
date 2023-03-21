da76c2c2162163a86b169f68397a0c793a2a6bc1
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-20 17:28:55 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this programme will print with puts and not printf
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char lt = 'a'
+
+	for (i = 0; i < 26; i++)
+	{
+		if (lt != e && lt != q)
+			putchar(tolower(lt + i));
+	}
+	putchar('\n');
+
+	return (0);
+}
