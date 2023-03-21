f21399bc9bf91e0aa0a15b324ff400cfb5966cfc
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-20 18:09:22 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this programme will print with puts and not printf
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(tolower('A' + i));
+	}
+	putchar('\n');
+
+	return (0);
+}
