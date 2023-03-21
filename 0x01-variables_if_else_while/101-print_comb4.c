05a0eb3e01237fae1383f5851d8f0ea060abbe3a
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-20 19:00:48 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: this programme will print nums from 0 to 10
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (i != j && i < j)
+				{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+					if (i + j + k != 24)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
