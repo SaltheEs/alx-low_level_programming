60e88738980f3297d84ac3032f79d0f3c3109838
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-21 19:32:37 -0700

    almost there again 2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,13 +5,14 @@
 */
 void times_table(void)
 {
-	int i, j;
+	int i, j, p;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			_putchar ((i + '0') * (j + '0') + '0');
+			p = i * j;
+			_putchar (p + '0');
 			_putchar (',');
 			_putchar (' ');
 			_putchar (' ');
