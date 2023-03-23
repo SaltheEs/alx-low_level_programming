6d8e60bf50c167eb8f1c81828fffbc23abc6edee
SaltheEs <salmaessoussi1994@gmail.com>
2023-03-22 18:29:28 -0700

    Modified 9 tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 			p = i * j;
 			_putchar ((p / 10) + '0');
 			_putchar ((p % 10) + '0');
-			if (i * 9 != i * j)
+			if (j != 9)
 			{
 				_putchar (',');
 				_putchar (' ');
