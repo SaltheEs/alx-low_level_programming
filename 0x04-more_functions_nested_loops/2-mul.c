86949776581713821f3b4b127d3651a28e714b6f
SaltheEs <salma.essoussi@gmail.com>
2023-03-23 16:15:36 -0700

    Created mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies a and b
+ * @a: the first number
+ * @b: the second number
+ *
+ * Return: the value of a multiplied by b
+*/
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
