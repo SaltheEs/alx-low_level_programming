d72cc76e7c7cc6376c343d1ed1274e1e86dd2bf0
SaltheEs <salma.essoussi@gmail.com>
2023-03-23 19:09:16 -0700

    Created mul function

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,14 +9,15 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
 	{
-		if (n > 0)
+		for (j = 0; j < i; j++)
 		{
-			for (j = 1; j <= i; j++)
-				_putchar (' ');
-			_putchar (92);
-			_putchar ('\n');
+			_putchar(' ');
 		}
+		_putchar(92);
+		_putchar('\n');
 	}
 }
