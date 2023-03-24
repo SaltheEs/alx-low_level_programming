88ee0e3b7090ea66226aa5a04f866d38752fe10f
SaltheEs <salma.essoussi@gmail.com>
2023-03-23 17:17:43 -0700

    Created print_most_numbers fonction

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,18 +4,18 @@
  * print_most_numbers - prints number from 0 to 9 exept 2 and 4
 */
 
-void print_most_numbers()
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i != 2 || i != 4)
+		if (i != 2 && i != 4)
 		{
 			_putchar ((i % 10) + '0');
 		}
 		else
-			continue:
+			continue;
 	}
 	_putchar ('\n');
 }
