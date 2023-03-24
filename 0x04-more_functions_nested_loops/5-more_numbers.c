64b362e9cbb16ba34b42f594d315265754b4e3b5
SaltheEs <salma.essoussi@gmail.com>
2023-03-23 17:38:35 -0700

    Created 14 printing function

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints number from 0 to 14 ten times
+*/
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; i <= 14; j++)
+			_putchar ((i % 10) + '0');
+		_putchar ('\n');
+	}
+	_putchar ('\n');
+}
