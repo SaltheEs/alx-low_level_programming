12ce987dfacdf0e952a1c2cb5295e431a1c3ba12
SaltheEs <salma.essoussi@gmail.com>
2023-03-28 19:36:52 -0700

    Created puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other char in a string
+ * @str: the string in question
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_pucthar('\n');
+}
