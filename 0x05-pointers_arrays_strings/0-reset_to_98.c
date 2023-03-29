df9a336c73839062c376900e85c1546d516d73bf
SaltheEs <salma.essoussi@gmail.com>
2023-03-28 18:24:27 -0700

    Updated pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-*reset_to_98 - changes the pointer value of n to 98
-*@n: the pointer to be changed
-*
+ * reset_to_98 - changes the value of *n to 98
+ * @n: the pointer's to be changed
+ *
 */
 
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
